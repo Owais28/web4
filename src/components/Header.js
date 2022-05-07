27ac6052ff2e1b0ad364d76c51314bad5afbcde3
Owais28 <owaisathar2882@gmail.com>
2022-05-07 12:54:54 +0530

    sd

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import React from 'react'
 import {FiMenu} from 'react-icons/fi'
 // Styles 
 import '../index.css'
-
+import heroImg from '../images/6217cc8fc720654e5d968fa0_home-hero-image-web3-webflow-ecommerce-template.png'
 export default function Header() {
   return (
     <header>
@@ -18,15 +18,18 @@ export default function Header() {
                      </li>
                  </ul>
              </nav>
-            <div className="container pt-5">
+            <div className="container pt-3">
                 <div className="grid-2">
                     <div className="hero-section">
                         <h1 className='font-display-1'>Buy growing crypto right from your phone</h1>
-                        <p>Buy growing crypto right from your phone</p>
-                        <div className="button-grp">
-                            <button>Get Started</button>
-                            <button>Brows Featers</button>
+                        <p>Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua ut dolor sit</p>
+                        <div className="btn-grp">
+                            <button className='btn btn-primary'>Get Started</button>
+                            <button className='btn btn-secondary'>Brows Featers</button>
                         </div>
+                    </div>
+                    <div className="hero-img">
+                        <img src={heroImg} alt=""/>
                     </div>
                 </div>
             </div>
